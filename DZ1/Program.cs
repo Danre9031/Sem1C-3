870109a205054de9402ae43585c4c45f6a9a0ffc
Mihail <Dante9041@mail.ru>
2022-06-20 09:55:37 +0300

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = Input("Введи пятизначное число:");
+int temp=a;
+int b = 0;
+
+while (temp !=0)
+{
+    b= b * 10 + temp % 10;
+    temp /= 10;
+}
+if (a==b)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
