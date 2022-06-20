d941171b940e558d05a55a5ff5b7aeef90bb64b0
Mihail <Dante9041@mail.ru>
2022-06-20 09:56:20 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = Input("Введите число: ");
+int i =2;
+int result = 1;
+string number ="1";
+while(n>=i)
+{
+    result= Quadro3(i);
+    number = number + "," +Convert.ToString(result);
+    i++;
+}
+Console.WriteLine(number);
+
+
+int Quadro3(int a)
+{
+    return a * a * a;
+}
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
